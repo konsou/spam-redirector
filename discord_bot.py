a776715649272dcff84fdc5e69ab65cfedb496d0
Tomi Javanainen <tomi.javanainen@gmail.com>
2021-10-03 20:26:10 +0300

    Save sent email ids to disk to avoid sending multiple copies

--- discord_bot.py
+++ discord_bot.py
@@ -1,15 +1,40 @@
 import asyncio
+import json
 
 import discord
 from discord.ext import tasks
 
 from settings import *
 
+
+SentIds = dict[int: int]
+
+
+def save_sent_ids(ids: SentIds) -> None:
+    print('Saving sent id\'s to disk...')
+    with open(SENT_IDS_FILE, 'w', encoding='utf-8') as f:
+        json.dump(ids, f)
+
+
+def load_sent_ids() -> SentIds:
+    print('Loading sent id\'s from disk...')
+    with open(SENT_IDS_FILE, encoding='utf-8') as f:
+        return json.load(f)
+
+
 if __name__ == '__main__':
     with open(DISCORD_TOKEN_FILE) as f:
         token = f.read().strip()
 
     client = discord.Client()
+
+    sent_ids: SentIds
+    try:
+        sent_ids = load_sent_ids()
+    except (FileNotFoundError, json.JSONDecodeError):
+        print(f'Creating new empty sent id\'s file...')
+        sent_ids = dict()
+        save_sent_ids(sent_ids)
 
     @client.event
     async def on_ready():
